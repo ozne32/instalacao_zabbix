8cfe261e08af64494df7b4fff6ea0175c1283e5e
ozne32 <enzorcc.mf3rs@gmail.com>
2024-08-12 19:59:51 -0300

    first commit

---
+++ inst_zabbix.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo 'coloque o ip do servidor'
+read ipServidor
+echo 'coloque o hostname'
+read hostname
+apt update 
+apt upgrade -y 
+#essa parte é a parte da documentação --> aqui está instalando para debian
+wget https://repo.zabbix.com/zabbix/7.0/debian/pool/main/z/zabbix-release/zabbix-release_7.0-2+debian12_all.deb  
+dpkg -i zabbix-release_7.0-2+debian12_all.deb 
+#vai até aqui
+apt update
+apt install zabbix-agent2 zabbix-agent2-plugin-* -y
+#mudando o /etc/zabbix/zabbix_agent2.conf
+sed -i "s/^Server=127.0.0.1/Server=$ipServidor/" /etc/zabbix/zabbix_agent2.conf 
+sed -i "s/^ServerActive=127.0.0.1/ServerActive=$ipServidor/" /etc/zabbix/zabbix_agent2.conf 
+sed -i "s/Hostname=Zabbix server/Hostname=$hostname/" /etc/zabbix/zabbix_agent2.conf
+#fim do /etc/zabbix/zabbix_agent2.conf
+#restartando os serviços
+systemctl restart zabbix-agent2  
+systemctl enable zabbix-agent2
+
